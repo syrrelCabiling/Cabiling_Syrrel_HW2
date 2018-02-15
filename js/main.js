e53fa35345ed65b929608606d325a4f09f4f1549
Syrrel Cabiling <syrrelcabiling@syrrels-mbp.fc.ca>
2018-02-15 10:35:24 -0500

    made changes to index and js files

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 (() =>)
 {
   console.log('fired');
+  // variables always go at the top
+  var iconSet = document.querySelectorAll('svg')
+  //functions here
+  function logThisIcon() {
+    console.log('clicked on this icon',this.id);
+
+    //changes opacity
+    this.style.opacity = 0.5;
+  }
+  //event handling goes at the bottom
+  iconSet.forEach(icon => icon.addEventListener('click',logThisIcon));
+
 })();
